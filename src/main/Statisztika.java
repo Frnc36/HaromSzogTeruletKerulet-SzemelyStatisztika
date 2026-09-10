448a1d33ec74f06c3ed8ab4a08d0b2f0728a1bd1
magori.ferenc <magori.ferenc@DESKTOP-4BPGBS5.akk.lan>
2026-09-10 16:02:44 +0200

    OOP osztalyok elkezdve2

--- src/main/Statisztika.java
+++ src/main/Statisztika.java
@@ -1,5 +1,35 @@
 package main;
 
+import java.time.LocalDate;
+
 public class Statisztika {
-    
+    private static void koszontes(String s) {
+        koszontes(s, false);
+    }
+
+    //private static String koszontes(String[] s, int velszam)
+    private static String koszontes(String[] s, boolean keveres) {
+        //return s[velszam];
+        String t[] = {"Szia", "Szius", "Hello"};
+        if (keveres) {
+            int i = (int) (Math.random() * t.length);
+        }
+    }
+
+    private static int eletKorSzamitas(int szulEv, LocalDate maiEv) {
+        return maiEv.getYear() - szulEv;
+    }
+
+    private static int nyugdijSzamitas(int eletkor) {
+        int i = 0;
+        while (eletkor < 65) {
+            eletkor++;
+            i++;
+        }
+        return i;
+    }
+
+    private static boolean pozitivSzam(int szulEv) {
+        return szulEv > 0;
+    }
 }
